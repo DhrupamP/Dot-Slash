af7885f79cfe7c441229088e018b324f96b49bd9
somyag <somyagu2305@gmail.com>
2022-01-08 12:30:19 +0530

    add ML model

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:try_notif/pages/homepage.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:try_notif/pages/singleHome.dart';
 
 void main() {
--- lib/pages/disease.dart
+++ lib/pages/disease.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:tflite/tflite.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 class DiseasePage extends StatefulWidget {
   const DiseasePage({Key? key}) : super(key: key);
 
@@ -8,8 +10,25 @@ class DiseasePage extends StatefulWidget {
 }
 
 class _DiseasePageState extends State<DiseasePage> {
+  late File _image;
+  late List _output;
+  final picker= ImagePicker();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        centerTitle: true,
+        title: Text('Crop Disease Identifier'),
+      ),
+      body: ListView(
+        children: [
+          GestureDetector(
+
+          ),
+        ],
+      ),
+    );
   }
+
 }
